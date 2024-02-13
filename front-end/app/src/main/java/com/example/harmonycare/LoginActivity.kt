4e56b157af552037b89d2b599f91108159821bc7
k56754665 <k56754665@gmail.com>
2024-02-13 15:19:55 +0900

    login (authcode)

--- front-end/app/src/main/java/com/example/harmonycare/LoginActivity.kt
+++ front-end/app/src/main/java/com/example/harmonycare/LoginActivity.kt
@@ -1,138 +1,57 @@
 package com.example.harmonycare
 
-import android.accounts.AccountManager
-import android.accounts.AccountManagerCallback
-import android.accounts.AccountManagerFuture
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.content.IntentSender
-import android.credentials.GetCredentialException
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.Message
-import android.util.Log
-import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.credentials.GetCredentialRequest
-import androidx.credentials.GetPasswordOption
-import androidx.credentials.GetPublicKeyCredentialOption
-import androidx.credentials.CustomCredential
-import androidx.credentials.PasswordCredential
-import androidx.credentials.PublicKeyCredential
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.net.HttpURLConnection
-import java.net.URL
-import java.util.logging.Handler
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
-    val credentialManager= androidx.credentials.CredentialManager.create(this)
-    val getPasswordOption = GetPasswordOption()
-    val googleOAuthUrl = URL("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&access_type=offline&include_granted_scopes=true&response_type=code&redirect_uri=http://localhost:8080&client_id=185976520158-phphtutm302clototd3rqgecng4a4bg2.apps.googleusercontent.com\")\n")
+    private lateinit var sharedPreferences: SharedPreferences
 
-    /*// Get passkey from the user's public key credential provider.
-    val getPublicKeyCredentialOption = GetPublicKeyCredentialOption(
-        requestJson = requestJson
-    )
-    val getCredRequest = GetCredentialRequest(
-        listOf(getPasswordOption, getPublicKeyCredentialOption)
-    )
-
-
-    val am: AccountManager = AccountManager.get(this)
-    val options = Bundle()
-
-    fun getToken() {
-        val options = null // Authenticator-specific options
-
-        am.getAuthToken(
-            googleOAuthUrl,                     // Account retrieved using getAccountsByType()
-            "Manage your tasks",            // Auth scope
-            options,                        // Authenticator-specific options
-            this,                           // Your activity
-            OnTokenAcquired(),              // Callback called when a token is successfully acquired
-            Handler(OnError())              // Callback called if an error occurs
-        )
-    }
-
-    private inner class OnTokenAcquired : AccountManagerCallback<Bundle> {
-
-        override fun run(result: AccountManagerFuture<Bundle>) {
-            val launch: Intent? = result.getResult().get(AccountManager.KEY_INTENT) as? Intent
-            if (launch != null) {
-                startActivityForResult(launch, 0)
-            }
-        }
-    }
-
-    private inner class OnError : Handler.Callback {
-        override fun handleMessage(msg: Message): Boolean {
-            // Handle error here
-            Log.e(TAG, "Error occurred during authentication")
-            return true
-        }
-    }*/
-
-    fun sendRequest(token: String) {
-        val url = URL("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&access_type=offline&include_granted_scopes=true&response_type=code&redirect_uri=http://localhost:8080&client_id=185976520158-phphtutm302clototd3rqgecng4a4bg2.apps.googleusercontent.com")
-        val conn = url.openConnection() as HttpURLConnection
-
-        conn.apply {
-            addRequestProperty("client_id", "your_client_id")
-            addRequestProperty("client_secret", "your_client_secret")
-            setRequestProperty("Authorization", "OAuth $token")
-        }
-    }
-
-
-    /*private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
-    private val REQ_ONE_TAP = 200
-    private var showOneTapUI = true*/
-    /*val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
-        .setFilterByAuthorizedAccounts(true)
-        .setServerClientId(SERVER_CLIENT_ID)
-        .build()*/
-
-
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        /*oneTapClient = Identity.getSignInClient(this)
-        signInRequest = BeginSignInRequest.builder()
-            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-                .setSupported(true)
-                .build())
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(getString(R.string.google_server_client_id))
-                    // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(false)//true를 하면 인증계정으로만 필터링함
-                    .build())
-            // Automatically sign in when exactly one credential is retrieved.
-            .setAutoSelectEnabled(true)
-            .build()*/
-        /*val loginButton: ImageButton = findViewById(R.id.GoogleLoginButton)
-                loginButton.setOnClickListener {
-                    showLoginDialog()
-                }*/
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // SharedPreferences 초기화
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
         webView = findViewById(R.id.webView)
 
         // WebView 설정
-        webView.webViewClient = WebViewClient()
-        webView.settings.userAgentString = "Mozilla/5.0 AppleWebKit/535.19 Chrome/121.0.0 Mobile Safari/535.19";
+        webView.settings.userAgentString = "Mozilla/5.0 AppleWebKit/535.19 Chrome/121.0.0 Mobile Safari/535.19"
         webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null && url.startsWith("http://localhost:8080")) {
+                    // 리다이렉션 링크를 가져와서 파라미터 추출
+                    showToast("리다이렉션 링크: $url")
+                    val authCode = extractCodeFromUrl(url)
+                    if (authCode != null) {
+                        // SharedPreferences에 authcode 저장
+                        saveAuthCode(authCode)
+                        showToast("인증 코드 저장됨: $authCode")
+
+                        // MainActivity로 이동
+                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                        // 현재 액티비티 종료
+                        finish()
+                    } else {
+                        showToast("인증 코드가 없습니다.")
+                    }
+                    return true // 리다이렉션된 링크 처리를 위해 true 반환
+                }
+                // 다른 URL은 기본적인 방식으로 처리
+                return super.shouldOverrideUrlLoading(view, url)
+            }
+        }
 
         // Google 로그인 페이지 URL 로드
         val url = "https://accounts.google.com/o/oauth2/v2/auth?" +
@@ -143,19 +62,28 @@ class LoginActivity : AppCompatActivity() {
                 "redirect_uri=http://localhost:8080&" +
                 "client_id=185976520158-phphtutm302clototd3rqgecng4a4bg2.apps.googleusercontent.com"
         webView.loadUrl(url)
-
     }
 
-    // 자격 증명을 가져오는 과정에서 예외가 발생했을 때의 처리를 담은 함수를 정의합니다.
-    private fun handleFailure(e: GetCredentialException) {
-        // GetCredentialException 예외를 처리하는 코드를 작성합니다.
-        Log.e(TAG, "Error retrieving credential: ${e.message}")
+    private fun extractCodeFromUrl(url: String): String? {
+        val pattern = Pattern.compile("\\bcode=([^&]+)")
+        val matcher = pattern.matcher(url)
+
+        return if (matcher.find()) {
+            matcher.group(1)
+        } else {
+            null
+        }
     }
 
-    companion object {
-        private const val TAG = "LoginActivity"
+
+    private fun saveAuthCode(authCode: String) {
+        // SharedPreferences에 authcode 저장
+        val editor = sharedPreferences.edit()
+        editor.putString("authcode", authCode)
+        editor.apply()
     }
 
-
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
-
--- front-end/app/src/main/java/com/example/harmonycare/MainActivity.kt
+++ front-end/app/src/main/java/com/example/harmonycare/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.harmonycare
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -12,12 +15,27 @@ import com.example.harmonycare.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // SharedPreferences 초기화
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        // SharedPreferences에서 authcode 가져오기
+        val authCode = sharedPreferences.getString("authcode", null)
+
+        if (authCode != null) {
+            // authcode가 존재하면 토스트 메시지로 표시
+            showToast("인증 코드: $authCode")
+        } else {
+            // authcode가 존재하지 않으면 토스트 메시지로 표시
+            showToast("인증 코드가 없습니다.")
+        }
 
         val navView: BottomNavigationView = binding.navView
 
@@ -30,5 +48,9 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
